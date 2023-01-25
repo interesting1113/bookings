e6084d4a6a42399a78605f498fbc42e9f097bab9
interesting1113 <sylviayang1113@gmail.com>
2023-01-25 15:36:26 +0900

    setting templates

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ const port = ":8080"
 
 func main() {
 	http.HandleFunc("/", Home)
-	http.HandleFunc("/", About)
+	http.HandleFunc("/about", About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s:", port))
 	_ = http.ListenAndServe(port, nil)
